7afc333c515b2861720e32988ddb0c8e57faa3ed
Orianamartina <orianapettinelli04@gmail.com>
2023-04-23 23:11:38 -0300

    db

--- PF-App-main/Api/src/db.js
+++ PF-App-main/Api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize, HasMany } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
+//const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
 //Conexion  a la Base de Datos
 
@@ -17,13 +17,44 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 // });
 
 //Deploy
-const sequelize = new Sequelize(DB_DEPLOY,{
+/*const sequelize = new Sequelize(DB_DEPLOY,{
     logging: false,
     native: false,
     define: {
     timestamps: false
 
-}})
+}})*/
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+} = process.env;
+
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: DB_PORT,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+        keepAlive: true,
+      },
+      ssl: true,
+    })
+    : new Sequelize(
+      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+      { logging: false, native: false }
+    );
 
 const basename = path.basename(__filename);
 
