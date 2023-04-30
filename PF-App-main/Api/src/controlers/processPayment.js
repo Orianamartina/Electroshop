b090bd41edc9431fbd1161e7cb31894003e1b2f9
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-04-30 12:05:47 -0300

    descuento aplicado a mercado pago

--- PF-App-main/Api/src/controlers/processPayment.js
+++ PF-App-main/Api/src/controlers/processPayment.js
@@ -7,6 +7,28 @@ mercadopago.configure({ access_token: process.env.MERCADOPAGO_KEY });
 const payment = async (req, res) => {
   const products = req.body.items;
   const id = req.body.userId;
+  const discount = req.body.discount
+
+  console.log(discount, "este es el descuento que llega la back")
+
+
+  if( discount ){
+    let saleDiscount = {
+      id: "desc",
+        title:"descuento" ,
+        brand: "descuento",
+        picture_url: null,
+        description: "descuento de la compra actual",
+        category_id: "descuento",
+        quantitySold: 0,
+        stock: 1,
+        disable: false,
+        price: discount,
+        ShoppingCart_Products: { quantity: 1, }
+      };
+      products.push(saleDiscount);
+    }
+    
   const preference = await {
     items: products.map((p) => {
       return {
@@ -33,8 +55,10 @@ const payment = async (req, res) => {
 
   mercadopago.preferences
     .create(preference)
-    .then((response) => res.status(200).send({ response }))
-    .catch((error) => res.status(400).send({ error: error.message }));
+    .then((response) => {
+      res.status(200).send({ response })})
+    .catch((error) => {
+      res.status(400).send({ error: error.message })});
 };
 
 module.exports = payment;
--- PF-App-main/Client/src/components/PurchaseOrderButton/PurchaseOrderButton.jsx
+++ PF-App-main/Client/src/components/PurchaseOrderButton/PurchaseOrderButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import axios from "axios";
 
-const PurchaseOrderButton = ({ products, user, shippingData }) => {
+const PurchaseOrderButton = ({ products, user, shippingData, discount }) => {
   const API_URL = "payment";
+  console.log(discount, "este es el decuento que llega a PurchaseOrder")
 
   const handlePayment = () => {
-    axios.post(API_URL, { items: [...products], userId: user }).then((res) => {
+    axios.post(API_URL, { items: [...products], userId: user, discount }).then((res) => {
       localStorage.setItem("shippingData", JSON.stringify(shippingData));
       window.location.href = res.data.response.body.init_point;
     });
