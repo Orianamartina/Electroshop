c3fa6fe0c2d7a4e6f747b31d12d7e852bb3ce591
Orianamartina <88673684+Orianamartina@users.noreply.github.com>
2023-04-30 23:19:55 -0300

    Update mail.config.js

--- PF-App-main/Api/src/config/mail.config.js
+++ PF-App-main/Api/src/config/mail.config.js
@@ -7,6 +7,9 @@ const EMAIL = process.env.EMAIL;
 const EMAIL_PSSWRD = process.env.EMAIL_PSSWRD;
 const SERVER = "https://electroshop-production.up.railway.app/"
 const CLIENT_HOST = "https://electroshop-delta.vercel.app/"
+//const SERVER = process.env.SERVER
+//const CLIENT_HOST = process.env.CLIENT_HOST
+
 
 const mail = {
   user: EMAIL,
