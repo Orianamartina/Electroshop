7550cfb1b8fb9bdbb2cdced1040cf0752d6e89db
Orianamartina <88673684+Orianamartina@users.noreply.github.com>
2023-05-02 13:01:20 -0300

    Update index.js

--- PF-App-main/Api/index.js
+++ PF-App-main/Api/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 // Syncing all the models at once.
 
-conn.sync({ force: true }).then(() => { 
+conn.sync({ force: false }).then(() => { 
   server.listen(PORT, () => {
     serverAdmin();
     addProducts();
